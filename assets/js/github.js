9476c57b323e83fe81418c544d9dd86df031b3c2
Roberto aguero <rjaguero00@gmail.com>
2018-09-10 12:29:52 -0700

    fetch and display repos

--- assets/js/github.js
+++ assets/js/github.js
@@ -2,16 +2,22 @@ class Github {
     constructor() {
         this.client_id = '2ea79e78adbec6904a24';
         this.client_secret = 'd9a1bdad91df8f88589eb42dd22c5785f844b26b';
+        this.repos_count = 5;
+        this.repos_sort = 'create: asc';
     }
 
     //Get user method
     async getUser(user) {
-        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}& client_secret=${this.client_secret}`);
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return {
-            profile
+            profile,
+            repos
         }
     }
 }
