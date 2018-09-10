36be5a7c59c40fcf8e2640fa4471073385e5de05
Roberto aguero <rjaguero00@gmail.com>
2018-09-10 11:19:04 -0700

    get method

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,27 @@
-//JS
+//Init Github
+const github = new Github;
+
+//Search Input
+const searchUser = document.getElementById('searchUser');
+
+//Search Input event listener
+searchUser.addEventListener('keyup', (e) => {
+    //Get input text
+    const userText = e.target.value;
+
+    if (userText !== '') {
+        //Make HTTP call
+        github.getUser(userText)
+            .then(data => {
+                if (data.profile.message === 'Not Found') {
+                    //Show Alert
+
+                } else {
+                    //Show profile
+
+                }
+            })
+    } else {
+        //Clear Profile
+    }
+});
--- assets/js/github.js
+++ assets/js/github.js
@@ -1,1 +1,17 @@
-//JS
+class Github {
+    constructor() {
+        this.client_id = '2ea79e78adbec6904a24';
+        this.client_secret = 'd9a1bdad91df8f88589eb42dd22c5785f844b26b';
+    }
+
+    //Get user method
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}& client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+
+        return {
+            profile
+        }
+    }
+}
