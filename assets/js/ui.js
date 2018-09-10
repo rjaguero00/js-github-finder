9b7a6051dd84858308c0f822476660f9a66a4055
Roberto aguero <rjaguero00@gmail.com>
2018-09-10 12:05:44 -0700

    add alert message

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -2,7 +2,7 @@ class UI {
     constructor() {
         this.profile = document.getElementById('profile');
     }
-
+    //Display profile in UI
     showProfile(user) {
         this.profile.innerHTML = `
         <div class="card card-body mb-3">
@@ -30,5 +30,44 @@ class UI {
         <div id="repos"></div>
         `;
     }
+    //Show alert message
+    showAlert(message, className) {
+        //Clear any remaining alerts
+        this.clearAlert();
 
+        //Create div
+        const div = document.createElement('div');
+        //Add Classes
+        div.className = className;
+        //Add text
+        div.appendChild(document.createTextNode(message));
+        //Get parent
+        const container = document.querySelector('.searchContainer');
+        //Get search box
+        const search = document.querySelector('.search');
+        //Insert Alert
+        container.insertBefore(div, search);
+
+        //Timeout after 3 seconds
+        setTimeout(() => {
+            this.clearAlert();
+        }, 3000);
+
+    }
+    //Clear alert message
+    clearAlert() {
+        const currentAlert = document.querySelector('.alert');
+
+        if (currentAlert) {
+            currentAlert.remove();
+        } else {
+
+        }
+    }
+
+
+    //Clear profile search
+    clearProfile() {
+        this.profile.innerHTML = '';
+    }
 }
